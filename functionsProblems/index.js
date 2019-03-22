94a661c39e62ae313e5602e2b950fc3fa646817b
Benjamin <hoppeconnect@gmail.com>
2019-03-22 12:50:09 -0400

    working on functions

---
+++ functionsProblems/index.js
@@ -0,0 +1,18 @@
+// function isEven(x) {
+//   if (x % 2 === 0) {
+//   return true;
+//   } else {
+//     return false
+// }ndnd
+
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+function factorial(num) {
+  var result = 1;
+  for (var i = 2; i <= num; i++) {
+    result *= i;
+  }
+  return result
+}
