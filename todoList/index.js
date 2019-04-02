2f2b96a50d035c826112726d70375dfff59841b7
Benjamin <hoppeconnect@gmail.com>
2019-04-02 09:39:51 -0400

    added more logic to todo list

--- todoList/index.js
+++ todoList/index.js
@@ -6,14 +6,36 @@ var input = prompt("What would you like to do");
 
 while(input !== "quit"){
   if(input === "list") {
-    console.log(todos);
+    listTodos();
   } else if(input === "new") {
-    var newTodo = prompt("Enter new todo");
-    todos.push(newTodo);
+    addTodo();
+  } else if(input ==="delete") {
+    deleteTodo();
   }
   input = prompt("What would you like to do");
 }
-
-
 console.log("Ok, you quit the app");
 }, 500);
+
+// Logic
+
+function listTodos() {
+  console.log('**********');
+  todos.forEach(function(todo, i){
+  console.log(i + ": " + todo);
+});
+  console.log('**********');
+};
+
+function addTodo() {
+  var newTodo = prompt("Enter new todo");
+  todos.push(newTodo);
+  console.log("Item added to list");
+}
+
+function deleteTodo() {
+  var index = prompt("enter index of todo to delte");
+  todos.splice(index, 1);
+  console.log("Item deleted from list");
+
+}
